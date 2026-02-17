d71535d5a0a102a31c201192c998ff41143851a8
LucianaBritoDev <lucianabritodev@gmail.com>
2026-02-17 20:34:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+        document.getElementById('bookingForm').addEventListener('submit', function(e) {
+            e.preventDefault();
+            alert('Reserva solicitada! Entraremos em contato em breve.');
+        });
+    </script>
